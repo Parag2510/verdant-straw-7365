331d01d4c70ec2091a974cc26449876dbbe067db
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2022-09-28 19:15:46 +0530

    new

---
+++ components/FOOTER.js
@@ -0,0 +1,71 @@
+const footer=()=>{
+    return `
+    <table class="tab">
+    <thead>
+        <tr>
+            <th class="bold"> INFORMATION </th>
+            <th class="bold" >CUSTOMER CARE</th>
+            <th class="bold" >NEWSLETTER</th> 
+        </tr>
+    
+    </thead>
+    <tbody>
+        <tr>
+         <td> <a class="box" href="https://www.fabbag.com/pages/about-us" >  About Us </a></td> 
+        <td>Call : <b>022-6805-6010</b>  <br>
+        (Monday to Friday : 9 AM - 7 PM).</td>
+         <td>Signup for the latest offers, updates and news
+        </td> 
+        </tr>
+        <tr>
+            <td> <a class="box" href="https://www.fabbag.com/pages/associated-brands">  Associted Brands </a> </td>
+            <td id="ho">Mail : crew@fabbag.com
+            </td>
+            <td> <input type="text" placeholder="Your email address">
+                <button id="mayur_btn">SUBSCRIBE</button></td></td>
+        </tr>
+        <tr>
+            <td> <a class="box"  href="https://www.fabbag.com/pages/faq"> FAQ </a> </td>
+            <td></td>
+            <td>Dont worry we dont spam
+            </td>
+        </tr>
+        <tr>
+            <td>  <a  class="box" href="https://www.fabbag.com/pages/collaborate-with-fab-bag">Collaborate With Fab Bag</a></td>
+            
+        </tr>
+        <tr>
+            <td  > <a class="box" href="https://www.fabbag.com/pages/contact-us"> Contact Us</a></td>
+        </tr>
+        <tr>
+            <td><a class="box" href="https://www.fabbag.com/pages/returns-and-refunds" >Returns & Refunds </a> </td>
+            
+        </tr>
+        <tr>
+            <td> <a class="box"   href="https://www.fabbag.com/pages/terms-and-conditions"> Terms & Conditions </a></td>
+        </tr>
+        <tr>
+            <td> <a class="box" href="https://www.fabbag.com/pages/privacy-policy"> Privacy Policy </a> </td>
+        </tr>
+    </tbody>
+    </table>
+
+<div>             
+
+<div class="class1">
+    <i id="a" class="fa-brands fa-facebook"></i>
+    <i id="b" class="fa-brands fa-instagram"></i>
+    <i id="c" class="fa-brands fa-twitter"></i>
+    <i id="d" class="fa-brands fa-youtube"></i>
+  </div>
+  
+    <h5>Copyright © 2022 fabbag.com All rights reserved.
+    </h5>   
+   
+  </div>
+  
+</div>                         
+    
+`
+}
+export default footer
