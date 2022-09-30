9d6d0c04f5085be6651c7cd90bc6b00be99bc8d7
sohel <mr.sohelsyed@gmail.com>
2022-09-30 23:35:52 +0530

    done

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,12 +1,18 @@
 //catch element and add event for cart open and close
-document.getElementById("cart_logo").addEventListener("click", opencart);
+// document.getElementById("cart_logo").addEventListener("click", opencart);
 
-function opencart() {
-  document.getElementById("mycart").style.width = "350px";
-}
+// function opencart() {
+//   document.getElementById("mycart").style.width = "350px";
+// }
 
-document.getElementById("closecart").addEventListener("click", closecart);
+// document.getElementById("closecart").addEventListener("click", closecart);
 
-function closecart() {
-  document.getElementById("mycart").style.width = "0";
-}
+// function closecart() {
+//   document.getElementById("mycart").style.width = "0";
+// }
+
+// import navar
+
+// import navbar from "../components/navbar";
+
+// document.getElementById("");
