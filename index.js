2e5c3be40f04ab550f3cfab4abaffe3f1be01b56
Vivek377 <vk640890@gmail.com>
2022-10-01 17:35:15 +0530

    done

--- index.js
+++ index.js
@@ -92,8 +92,9 @@ let nav = document.getElementById("navbar")
 
 nav.innerHTML = navbar()
 
-import footer from "./components/FOOTER.js";
+import footer from "./Footer/FOOTER.js";
 
 let foot = document.getElementById("footer")
 
-foot.innerHTML = footer()
+foot.innerHTML = footer()                         
+console.log(footer);
